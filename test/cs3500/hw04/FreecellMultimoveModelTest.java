106233dd62646a665ad144cb74329c0c82fa2a2e
Josh Pensky <joshuapensky@gmail.com>
2017-06-02 17:01:36 -0400

    Added tests for multimove

--- test/cs3500/hw04/FreecellMultimoveModelTest.java
+++ test/cs3500/hw04/FreecellMultimoveModelTest.java
@@ -376,6 +376,152 @@ public class FreecellMultimoveModelTest {
     fcm.move(PileType.CASCADE, 0, 6, PileType.CASCADE, 0);
   }
 
+  @Test(expected = IllegalArgumentException.class)
+  public void moveMultipleCardsToFoundation() {
+    fcm.startGame(Utils.reverse(fcm.getDeck()), 8, 4, false);
+    fcm.move(PileType.CASCADE, 0, 3, PileType.FOUNDATION, 0);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void moveMultipleCardsCantFromFoundation() {
+    fcm.startGame(Utils.reverse(fcm.getDeck()), 8, 4, false);
+    for (int i = 0; i <= 6; i++) {
+      fcm.move(PileType.CASCADE, 0, 6 - i, PileType.FOUNDATION, 0);
+      if (6 - (i + 1) >= 0) {
+        fcm.move(PileType.CASCADE, 4, 6 - (i + 1), PileType.FOUNDATION, 0);
+      }
+    }
+    fcm.move(PileType.FOUNDATION, 0, 10, PileType.CASCADE, 0);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void moveMultipleCardsToOpen() {
+    fcm.startGame(Utils.reverse(fcm.getDeck()), 8, 4, false);
+    fcm.move(PileType.CASCADE, 0, 3, PileType.OPEN, 0);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void moveMultipleCardsInvalidBuild() {
+    fcm.startGame(Utils.reverse(fcm.getDeck()), 8, 4, false);
+    fcm.move(PileType.CASCADE, 0, 3, PileType.CASCADE, 1);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void moveMultipleCardsToWrongSuit() {
+    fcm.startGame(Utils.reverse(fcm.getDeck()), 8, 4, false);
+
+    fcm.move(PileType.CASCADE, 1, 6, PileType.FOUNDATION, 0);
+    fcm.move(PileType.CASCADE, 0, 6, PileType.CASCADE, 5);
+    fcm.move(PileType.CASCADE, 5, 5, PileType.CASCADE, 1);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void moveMultipleCardsToWrongValue() {
+    fcm.startGame(Utils.reverse(fcm.getDeck()), 8, 4, false);
+
+    fcm.move(PileType.CASCADE, 1, 6, PileType.FOUNDATION, 0);
+    fcm.move(PileType.CASCADE, 0, 6, PileType.CASCADE, 5);
+    fcm.move(PileType.CASCADE, 5, 5, PileType.CASCADE, 3);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void moveMultipleCardsExceedsMaxBuildSizeEmptyOpens() {
+    fcm.startGame(Utils.reverse(fcm.getDeck()), 8, 4, false);
+    fcm.move(PileType.CASCADE, 0, 6, PileType.CASCADE, 5);
+    fcm.move(PileType.CASCADE, 5, 5, PileType.CASCADE, 0);
+    fcm.move(PileType.CASCADE, 0, 5, PileType.CASCADE, 5);
+    fcm.move(PileType.CASCADE, 5, 4, PileType.CASCADE, 0);
+    fcm.move(PileType.CASCADE, 0, 4, PileType.CASCADE, 5);
+    fcm.move(PileType.CASCADE, 5, 3, PileType.CASCADE, 0);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void moveMultipleCardsExceedsMaxBuildSizeFilledOpens() {
+    fcm.startGame(Utils.reverse(fcm.getDeck()), 8, 4, false);
+    // fill empty slots
+    fcm.move(PileType.CASCADE, 1, 6, PileType.OPEN, 0);
+    fcm.move(PileType.CASCADE, 2, 6, PileType.OPEN, 1);
+    fcm.move(PileType.CASCADE, 3, 6, PileType.OPEN, 2);
+    fcm.move(PileType.CASCADE, 4, 5, PileType.OPEN, 3);
+    // move one card
+    fcm.move(PileType.CASCADE, 0, 6, PileType.CASCADE, 5);
+    // move two cards
+    fcm.move(PileType.CASCADE, 5, 5, PileType.CASCADE, 0);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void moveMultipleCardsExceedsMaxBuildSizeEmptyCascades() {
+    fcm.startGame(Utils.reverse(fcm.getDeck()), 8, 4, false);
+    for (int i = 0; i <= 6; i++) {
+      fcm.move(PileType.CASCADE, 0, 6 - i, PileType.FOUNDATION, 0);
+      if (6 - (i + 1) >= 0) {
+        fcm.move(PileType.CASCADE, 4, 6 - (i + 1), PileType.FOUNDATION, 0);
+      }
+    }
+    fcm.move(PileType.FOUNDATION, 0, 12, PileType.CASCADE, 0);
+    // empty cascades is 1, max build size should be (4 + 1) * 2^1 = 10
+    fcm.move(PileType.CASCADE, 1, 6, PileType.CASCADE, 7); //1
+    fcm.move(PileType.CASCADE, 7, 5, PileType.CASCADE, 1); //2
+    fcm.move(PileType.CASCADE, 1, 5, PileType.CASCADE, 7); //3
+    fcm.move(PileType.CASCADE, 7, 4, PileType.CASCADE, 1); //4
+    fcm.move(PileType.CASCADE, 1, 4, PileType.CASCADE, 7); //5
+    fcm.move(PileType.CASCADE, 7, 3, PileType.CASCADE, 1); //6
+    fcm.move(PileType.CASCADE, 1, 3, PileType.CASCADE, 7); //7
+    fcm.move(PileType.CASCADE, 7, 2, PileType.CASCADE, 1); //8
+    fcm.move(PileType.CASCADE, 1, 2, PileType.CASCADE, 7); //9
+    fcm.move(PileType.CASCADE, 7, 1, PileType.CASCADE, 1); //10
+    fcm.move(PileType.CASCADE, 1, 1, PileType.CASCADE, 7); //11 (exceeds)
+  }
+
+  @Test
+  public void moveMultipleCardsToEmptyCascades() {
+    fcm.startGame(Utils.reverse(fcm.getDeck()), 8, 4, false);
+    for (int i = 0; i <= 6; i++) {
+      fcm.move(PileType.CASCADE, 0, 6 - i, PileType.FOUNDATION, 0);
+      if (6 - (i + 1) >= 0) {
+        fcm.move(PileType.CASCADE, 4, 6 - (i + 1), PileType.FOUNDATION, 0);
+      }
+    }
+    fcm.move(PileType.CASCADE, 1, 6, PileType.CASCADE, 7);
+    fcm.move(PileType.CASCADE, 7, 5, PileType.CASCADE, 1);
+    fcm.move(PileType.CASCADE, 1, 5, PileType.CASCADE, 7);
+    fcm.move(PileType.CASCADE, 7, 4, PileType.CASCADE, 1);
+    fcm.move(PileType.CASCADE, 1, 4, PileType.CASCADE, 7);
+    fcm.move(PileType.CASCADE, 7, 3, PileType.CASCADE, 4);
+    assertEquals("F1: A♠, 2♠, 3♠, 4♠, 5♠, 6♠, 7♠, 8♠, 9♠, 10♠, J♠, Q♠, K♠\nF2:\nF3:\nF4:\n"
+        + "O1:\nO2:\nO3:\nO4:\n"
+        + "C1:\n"
+        + "C2: K♥, J♥, 9♥, 7♥\n"
+        + "C3: K♦, J♦, 9♦, 7♦, 5♦, 3♦, A♦\n"
+        + "C4: K♣, J♣, 9♣, 7♣, 5♣, 3♣, A♣\n"
+        + "C5: 6♣, 5♥, 4♣, 3♥, 2♣, A♥\n"
+        + "C6: Q♥, 10♥, 8♥, 6♥, 4♥, 2♥\n"
+        + "C7: Q♦, 10♦, 8♦, 6♦, 4♦, 2♦\n"
+        + "C8: Q♣, 10♣, 8♣",
+        fcm.getGameState());
+  }
+
+  @Test
+  public void moveMultipleCardsBackAndForth() {
+    fcm.startGame(Utils.reverse(fcm.getDeck()), 8, 4, false);
+    fcm.move(PileType.CASCADE, 0, 6, PileType.CASCADE, 5);
+    fcm.move(PileType.CASCADE, 5, 5, PileType.CASCADE, 0);
+    fcm.move(PileType.CASCADE, 0, 5, PileType.CASCADE, 5);
+    fcm.move(PileType.CASCADE, 5, 4, PileType.CASCADE, 0);
+    fcm.move(PileType.CASCADE, 0, 4, PileType.CASCADE, 5);
+    assertEquals("F1:\nF2:\nF3:\nF4:\n"
+        + "O1:\nO2:\nO3:\nO4:\n"
+        + "C1: K♠, J♠, 9♠, 7♠\n"
+        + "C2: K♥, J♥, 9♥, 7♥, 5♥, 3♥, A♥\n"
+        + "C3: K♦, J♦, 9♦, 7♦, 5♦, 3♦, A♦\n"
+        + "C4: K♣, J♣, 9♣, 7♣, 5♣, 3♣, A♣\n"
+        + "C5: Q♠, 10♠, 8♠, 6♠, 4♠, 2♠\n"
+        + "C6: Q♥, 10♥, 8♥, 6♥, 5♠, 4♥, 3♠, 2♥, A♠\n"
+        + "C7: Q♦, 10♦, 8♦, 6♦, 4♦, 2♦\n"
+        + "C8: Q♣, 10♣, 8♣, 6♣, 4♣, 2♣",
+        fcm.getGameState());
+  }
+
   @Test
   public void moveCardToOpen() {
     fcm.startGame(fcm.getDeck(), 8, 4, false);
